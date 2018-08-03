109b12f39b60d849622e0e13d29a58ac1738447a
Abhishek Bhardwaj <abhishek@mail.com>
2018-08-03 12:53:49 +0530

    First commit

---
+++ WebApplication1/WebApplication1/Migrations/20180803061934_initialmigration.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NoteTitle = table.Column<string>(nullable: true),
+                    NoteContent = table.Column<string>(nullable: true),
+                    pinned = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "checklist",
+                columns: table => new
+                {
+                    checklistId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    listcontent = table.Column<string>(nullable: true),
+                    NoteId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_checklist", x => x.checklistId);
+                    table.ForeignKey(
+                        name: "FK_checklist_Note_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Note",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Labels",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true),
+                    NoteId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Labels", x => x.LabelId);
+                    table.ForeignKey(
+                        name: "FK_Labels_Note_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Note",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_checklist_NoteId",
+                table: "checklist",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_NoteId",
+                table: "Labels",
+                column: "NoteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "checklist");
+
+            migrationBuilder.DropTable(
+                name: "Labels");
+
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteContent { get; set; }
+        public bool pinned { get; set; }
+        public List<checklist> check { get; set; }
+        public List<Labels> labellist { get; set; }
+    }
+
+    public class checklist
+    {
+        [Key]
+       public int checklistId { get; set; }
+        public string listcontent { get; set; }
+    }
+
+    public class Labels
+    {
+        [Key]
+        public int LabelId { get; set; }
+        public string name { get; set; }
+    }
+}
